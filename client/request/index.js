750dc55dfe4aed9f5530e25bce40b83dfdb3439b
ypling <leonard.li.pub@gmail.com>
2016-12-20 12:36:04 -0800

    make request host configurable

--- client/request/index.js
+++ client/request/index.js
@@ -1,6 +1,10 @@
-const host = '/graphql';
+let _host = '/graphql';
+
+export function setHost(host){
+  _host = host + '/graphql';
+}
 export default function request(query, variables) {
-  return fetch(host, {
+  return fetch(_host, {
     method: 'post',
     headers: new Headers({
       "Content-Type": "application/json",
